ed1f3aeea7a759f63bf11a826ffbc51031680100
Duncan Graham <duncangraham75@gmail.com>
2016-08-03 11:21:55 -0700

    improved visuals

--- src/overlay.js
+++ src/overlay.js
@@ -31,7 +31,7 @@ function updateDebugOverlay(tracer, enabled) {
             return;
         }
 
-        let link, div;
+        let link, logo, div, count;
         let opts = tracer.options();
 
         // Make a guess about whether we are reporting to a dev instance or not
@@ -51,41 +51,91 @@ function updateDebugOverlay(tracer, enabled) {
         // cleared the BODY, etc.
         var overlay = document.getElementById(HOST_DIV_ID);
         if (!overlay) {
-            overlay = document.createElement("div");
-            overlay.id = HOST_DIV_ID;
-            overlay.style.position = "fixed";
-            overlay.style.bottom = "0";
-            overlay.style.right = "0";
-            overlay.style.width = "240px";
-            overlay.style.height = "120px";
-            overlay.style.padding = "4px";
-            overlay.style.border = "solid 1px #FFF";
-            overlay.style.backgroundColor = "#0060a2";
-            overlay.style.color = "#FFF";
-            overlay.style.fontSize = "12px";
-            overlay.style.fontFamily = "sans-serif";
-            overlay.style.opacity = "0.7";
+        
+            let container = document.createElement('div');
+            container.style.position = "fixed";
+            container.style.bottom = "2em";
+            container.style.right = "2em";
+            container.style.width = "3em";
+            container.style.height = "3em";
+            container.style.borderRadius = "3em";
+            container.style.cursor = "pointer";
+            container.style.background = 'rgb(0, 163, 255)';
+            container.style.boxShadow = '0 0 0 1px #c4c4c4, 0 2px 20px 0 #e2e2e2';
+            container.style.transition = '.5s all';
 
-            var close = document.createElement("div");
-            close.style.float = "right";
-            close.style.cursor = "pointer";
-            close.style.color = "#FFF";
-            close.onclick = function(evt) {
-                overlay.style.display = "none";
+
+            let btn = document.createElement('div');
+            btn.style.width = '100%';
+            btn.style.height = '100%';
+            btn.style.borderRadius = "3em";
+            btn.style.background = 'linear-gradient(to bottom right,#00d6ff,#3b6fcb 90%)';
+            btn.style.opacity = '0';
+            btn.style.boxShadow = '0 0 0 0 transparent';
+            btn.style.transition = '.5s all';
+
+            let badge = document.createElement('div');
+            badge.style.width = '.75em';
+            badge.style.height = '.75em';
+            badge.style.borderRadius = '1em';
+            badge.style.background = 'red';
+            badge.style.position = 'absolute';
+            badge.style.top = '0em';
+            badge.style.right = '0em';
+
+
+            container.onmouseenter = function(e) {
+              btn.style.background = 'linear-gradient(to bottom right,#00d6ff,#3b6fcb 90%)';
+              btn.style.boxShadow = 'inset 0 0 0 2px #FFF, inset 0 0 0 3px rgb(29, 184, 228)';
+              btn.style.opacity = '1'
+              container.style.boxShadow = 'rgb(196, 196, 196) 0px 0px 0px 1px, rgb(184, 184, 184) 0px 4px 30px 0px';
+              container.style.opacity = '1'
             };
-            close.innerHTML = "X";
-            overlay.appendChild(close);
+            
+            container.onmouseleave = function(e) {
+              btn.style.boxShadow = '0 0 0 0 transparent';
+              btn.style.opacity = '0';
+            container.style.boxShadow = '0 0 0 1px #c4c4c4, 0 2px 20px 0 #e2e2e2';
+              container.style.opacity = '.9'
+            };
 
+
+            logo = document.createElement('img');
+            logo.src = 'http://imgur.com/brQx4rK.png';
+            logo.style.width = '2em';
+            logo.style.position = 'absolute';
+            logo.style.top = '.925em';
+            logo.style.left = '.5em';
+
+            count = document.createElement('div');
+            count.style.fontSize = '.75em';
+            count.style.position = 'absolute';
+            count.style.top = '-.4rem';
+            count.style.right = '-.4rem';
+            count.style.minWidth = '1.2rem';
+            count.style.height = '1.2rem';
+            count.style.paddingTop = '.2rem';
+            count.style.borderRadius = '1.2em';
+            count.style.textAlign = 'center';
+            count.style.backgroundColor = '#ff7a7a';
+            count.style.color = '#FFF';
+
+
+            container.appendChild(btn);
+            container.appendChild(logo);
+            container.appendChild(count);
+
+            overlay = document.createElement("div");
+            var close = document.createElement("div");
             link = document.createElement("a");
             link.href = url;
-            link.style.color = "#FFF";
-            link.style.fontWeight = "bold";
-            link.appendChild( document.createTextNode("LightStep") );
             let title = document.createElement("div");
             title.appendChild(link);
+            title.appendChild(close);
             overlay.appendChild(title);
 
             document.body.appendChild(overlay);
+            document.body.appendChild(container);
         }
 
         let kMaxSpans = 4;
@@ -145,13 +195,17 @@ function updateDebugOverlay(tracer, enabled) {
 
             link = document.createElement("a");
             link.href = `${url}trace?span_guid=${span.span_guid}&at_micros=${span.youngest_micros}`;
-            link.style.color = "#FFF";
-            link.style.whiteSpace = "nowrap";
+            link.style.color = "#e0e0e0";
+            link.style.padding = '.5em';
+            link.style.lineHeight = '1.25em';
+            link.style.display = 'block';
             link.appendChild(document.createTextNode(joinedSpans[i].summary));
             div.appendChild(link);
 
             overlay.appendChild(div);
             totalLinks++;
+            console.log(totalLinks)
+            count.innerHTML = totalLinks;
         }
     });
 }
